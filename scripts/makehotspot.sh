242398f09b3f99109ea42ddcaea32729f0c0e320
xzvf <peter@bohner.me>
2021-10-08 15:51:02 +0200

    stuff

--- scripts/makehotspot.sh
+++ scripts/makehotspot.sh
@@ -2,7 +2,9 @@
 nmcli con add type wifi ifname wlo1 con-name Rhsp autoconnect yes ssid HailStallman
 nmcli con modify Rhsp wifi-sec.key-mgmt wpa-psk
 nmcli con modify Rhsp wifi-sec.psk "RichardStallman"
-nmcli con modify Rhsp 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
-nmcli con modify Rhsp ifname wlo1
+nmcli con modify Rhsp 802-11-wireless.mode ap
+nmcli con modify Rhsp 802-11-wireless.band bg
+nmcli con modify Rhsp ipv4.method shared
+nmcli con modify Rhsp ifname wlp2s0
 nmcli con up Rhsp
 
