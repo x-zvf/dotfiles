99217eb7987d1a3b4f3091e9167298de6a881d5c
xzvf <peter@bohner.me>
2021-10-08 14:52:43 +0200

    move nvm load to function to significanty decrease zsh loading times

--- scripts/shell/exports.sh
+++ scripts/shell/exports.sh
@@ -2,11 +2,13 @@
 
 export PATH="$PATH:/home/xzvf/.local/install/npm-global/bin:/bin:/home/xzvf/.local/bin:/home/xzvf/.gem/ruby/2.6.0/bin:/home/xzvf/inst/android-sdk/platform-tools/:/home/xzvf/.local/bin/flutter/bin:/home/xzvf/.gem/ruby/2.7.0/bin:/home/xzvf/.cargo/bin:/var/lib/snapd/snap/bin:/opt/sublime_text/:/opt/sublime_merge/:/opt"
 export ANDROID_HOME="$HOME/inst/android-sdk"
-export BROWSER="/usr/bin/firefox-developer-edition"
+export BROWSER="/usr/bin/firefox"
 export EDITOR='nvim'
 export VISUAL='nvim'
 #export MOZ_X11_EGL=1
 
+load-nvm(){
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
