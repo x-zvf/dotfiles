0436744ec4dc0d971aa2ac1bfe30c89d555b0ebd
xzvf (Péter Bohner) <peter@bohner.me>
2022-01-03 00:40:18 +0100

    Added progress bar to fixwifi script

--- scripts/fixwifi.sh
+++ scripts/fixwifi.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+echo '[     ]'
 sudo systemctl stop NetworkManager
+echo '[#    ]'
 sudo rmmod iwlmvm
+echo '[##   ]'
 sudo rmmod iwlwifi
+echo '[###  ]'
 sudo modprobe iwlmvm
+echo '[#### ]'
 sudo systemctl start NetworkManager
+echo '[#####]'
