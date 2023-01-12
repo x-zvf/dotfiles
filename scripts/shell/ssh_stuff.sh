f7d17a4e74d977dbe3a11e1c0e3b6f8c3b633e86
Péter Bohner (xzvf) <peter@bohner.me>
2023-01-12 11:42:26 +0100

    Make ssh agent init silent

--- scripts/shell/ssh_stuff.sh
+++ scripts/shell/ssh_stuff.sh
@@ -4,9 +4,9 @@
 SSH_ENV="$HOME/.ssh/environment"
 
 function start_agent {
-    echo "Initialising new SSH agent..."
+    #echo "Initialising new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-    echo succeeded
+    #echo succeeded
     chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
     #/usr/bin/ssh-add;
