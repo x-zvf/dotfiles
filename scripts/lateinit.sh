52852407bc3e0fa0d9b82e3994b8252ae28f9684
Péter Bohner (xzvf) <peter@bohner.me>
2023-12-27 20:50:27 +0100

    Added lateinit disable USB autosuspend

--- scripts/lateinit.sh
+++ scripts/lateinit.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 logger -t LATEINIT "late starting nmb/smb"
 sudo systemctl start nmb.service
 sudo systemctl start smb.service
 
+# while true;
+# do
+# 	hubstate="$(cat /sys/bus/usb/devices/1-4/power/control)"
+# 	mtstate="$(cat /sys/bus/usb/devices/1-5/power/control)"
+# 	if [ "$mtstate" != "on" ] || [ "$hubstate" != "on" ];
+# 	then
+# 		echo "Someone screwed with usb power: hub=$hubstate mt=$mtstate" | logger -t "lateinit - fixmouse"
+# 		echo on | sudo tee /sys/bus/usb/devices/1-4/power/control >/dev/null
+# 		echo on | sudo tee /sys/bus/usb/devices/1-5/power/control >/dev/null
+# 	fi
+# 	sleep 1
+# done
