936aef0db9c5ac9b8321c487304c3c11cc440a49
Péter Bohner (xzvf) <peter@bohner.me>
2025-11-24 14:04:21 +0100

    What feel's like the bianuall switch to sway

--- home/dot-config/nvim/init.lua
+++ home/dot-config/nvim/init.lua
@@ -306,6 +306,30 @@ require("lazy").setup({
       dependencies = { "nvim-lua/plenary.nvim" },
     },
     { "lewis6991/gitsigns.nvim", opts = {} },
+    {
+      "Olical/conjure",
+      ft = { "clojure", "fennel", "python" }, -- etc
+      lazy = true,
+      init = function()
+        -- Set configuration options here
+        -- Uncomment this to get verbose logging to help diagnose internal Conjure issues
+        -- This is VERY helpful when reporting an issue with the project
+        -- vim.g["conjure#debug"] = true
+      end,
+
+      -- Optional cmp-conjure integration
+      --dependencies = { "PaterJason/cmp-conjure" },
+    },
+    --{
+    --  "PaterJason/cmp-conjure",
+    --  lazy = true,
+    --  config = function()
+    --    local cmp = require("cmp")
+    --    local config = cmp.get_config()
+    --    table.insert(config.sources, { name = "conjure" })
+    --    return cmp.setup(config)
+    --  end,
+    --},
   },
   install = { colorscheme = { "carbonfox" } },
   checker = { enabled = true },
---
+++ home/dot-config/scripts/startup.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -x
+systemctl --user import-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP SWAYSOCK I3SOCK XCURSOR_SIZE XCURSOR_THEME NIXOS_OZONE_WL
+
+execifn() {
+    if [ -z "$(pgrep $1)" ];
+    then
+        shift
+        $@ &
+    fi
+}
+
+execifn dunst dunst
+systemctl --user restart kanshi.service
+
+pkill waybar; waybar &
+
+execifn pasystray pasystray
+execifn nm-applet nm-applet
+
+execifn wpaper wpaperd -d
+execifn kwalletd6 kwalletd6 -d
+
+execifn wl-paste wl-paste --watch cliphist store
+
+swaymsg 'workspace number 1; layout tabbed'
+execifn firefox firefox
+
+swaymsg 'workspace number 2; layout tabbed'
+
+execifn ferdium ferdium
+signal-desktop --password-store="kwallet6"
+execifn thunderbird thunderbird
+
+swaymsg 'workspace number 3'
+
+execifn nextcloud nextcloud
+
+if [ -z "$(pgrep swayidle)" ];
+then
+    swayidle -w before-sleep "loginctl lock-session $XDG_SESSION_ID" lock "playerctl -a pause && swaylock" &
+fi
+
+kanshictl reload
+
